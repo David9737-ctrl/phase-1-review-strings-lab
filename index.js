4f43d330681c7d5a718a4ae18f7907e0f06b083f
David <Marin9737@gmail.com>
2024-01-30 09:23:48 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+// // Write your code in this file!
+let currentUser = "Skippy"
+const initial = "S"
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!"
+let shortGreeting = "Welcome, " + initial + "!"
+
+
